760704c2d14a015375e2a4a9f87044b0a8fc8dd9
kylazath <kylazath@gmail.com>
2022-03-06 02:13:39 +0100

    Fix seeds typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Location.create(identifiable_by: '111.111.111.111',
+Locatable.create(identifiable_by: '111.111.111.111',
                 latitude: '52.199547', longitude: '20.997526')
 
-Location.create(identifiable_by: '222.222.222.222',
+Locatable.create(identifiable_by: '222.222.222.222',
                 latitude: '51.119403', longitude: '20.868495')
 
-Location.create(identifiable_by: '333.333.333.333',
+Locatable.create(identifiable_by: '333.333.333.333',
                 latitude: '50.300256', longitude: '18.682652')
